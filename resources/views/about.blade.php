e0686b82d83c2f01e5ebdee9e81ed34026898c01
AslanAV <autlevaslan@yandex.ru>
2022-10-11 22:30:42 +0300

    add layouts app.blade.php, add yield and section in layouts

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!-- Хранится в resources/views/about.blade.php -->
+
+@extends('layouts.app')
+
+
+@section('title', 'О блоге')
+
+
+@section('header', 'О блоге')
+
+@section('content')
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
