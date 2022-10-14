00c3d46cfe999c3edd22671e167ad9c1433e54fe
AslanAV <autlevaslan@yandex.ru>
2022-10-14 08:47:15 +0300

    add update CRUD

---
+++ resources/views/article/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    {{ Form::model($article, ['route' => ['articles.update', $article], 'method' => 'PATCH']) }}
+    @include('article.form')
+    {{ Form::submit('Обновить') }}
+    {{ Form::close() }}
+@endsection
